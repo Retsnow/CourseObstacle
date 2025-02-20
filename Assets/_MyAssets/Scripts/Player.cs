062407a6b11f7b215b7be7f59919367d038e3308
Retsnow <jeremysoleil5@gmail.com>
2025-02-20 15:51:26 -0500

    Camera Special + rendering

--- Assets/_MyAssets/Scripts/Player.cs
+++ Assets/_MyAssets/Scripts/Player.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float _playerSpeed = 10f;
+    [SerializeField] private float _playerSpeed = 10f;
+    [SerializeField] private float _rotationSpeed = 1000f;
 
     private void Update()
+    {
+        PlayerMovements();
+
+    }
+
+    private void PlayerMovements()
     {
         float dirX = Input.GetAxis("Horizontal");
         float dirZ = Input.GetAxis("Vertical");
@@ -13,10 +20,15 @@ public class Player : MonoBehaviour
 
         // Mormalise mon Vecteur avec une valeur maximale de 1
         direction.Normalize();
-        
-        transform.Translate(direction * Time.deltaTime * _playerSpeed);
+
+        transform.Translate(direction * Time.deltaTime * _playerSpeed, Space.World);
 
 
+
+        if (direction != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, _rotationSpeed * Time.deltaTime);
+        }
     }
-
 }
